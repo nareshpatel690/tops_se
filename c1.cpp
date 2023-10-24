9646065a107977e29ee5089798a6b5e9a2207f7f
nareshpatel690 <pnareshsp@gmail.com>
2023-10-24 17:05:06 +0530

    se

---
+++ c1.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+// # preposser
+// studio stands for input output and .h stands for header
+
+
+//functions
+
+/*
+there are two functions
+system defind
+
+with perameter
+without perameter
+
+user defind
+
+*/
+
+
+//in c languague all programs are exceuted from main function
+
+int main()
+
+
+{
+	
+	
+	
+	
+	printf("hello everyone my name is naresh patel");
+	return 0;
+}
